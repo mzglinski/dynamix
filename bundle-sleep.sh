7bef3b17dd08b31beb5fe59943a82bff61cd0cdd
Michał Zgliński <michal@cloudart.pl>
2025-04-15 21:37:17 +0200

    chore: update md5 file

--- bundle-sleep.sh
+++ bundle-sleep.sh
@@ -13,3 +13,11 @@ tar -cJf $script_dir/archive/dynamix.s3.sleep.txz .
 cd -
 
 rm -rf /tmp/pkg-s3-sleep
+
+# Calculate MD5 checksum
+MD5=$(md5sum $script_dir/archive/dynamix.s3.sleep.txz | awk '{print $1}')
+
+# Update the MD5 in the PLG file
+sed -i "s/<!ENTITY MD5       \"[^\"]*\">/<!ENTITY MD5       \"$MD5\">/" $script_dir/unRAIDv6/dynamix.s3.sleep.plg
+
+echo "Updated MD5 checksum to $MD5"
